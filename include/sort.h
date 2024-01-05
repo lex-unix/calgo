8265c218325179b12776d6ef978ace16f508e721
Lex Miin <alexmiyin@gmail.com>
2024-01-05 13:45:08 +0200

    feat: add heapsort

--- include/sort.h
+++ include/sort.h
@@ -2,7 +2,15 @@
 #define SORT_H
 
 void insertion_sort(int arr[], int n);
+
 void merge_sort(int arr[], int p, int r);
 void merge(int arr[], int p, int q, int r);
+
+int heap_parent(int i);
+int heap_left(int i);
+int heap_right(int i);
+void max_heapify(int arr[], int i, int heap_size);
+void build_max_heap(int arr[], int heap_size);
+void heapsort(int arr[], int heap_size);
 
 #endif
--- src/sort.c
+++ src/sort.c
@@ -62,3 +62,55 @@ void merge(int arr[], int p, int q, int r) {
 		k++;
 	}
 }
+
+int heap_parent(int i) {
+	return (i - 1) / 2;
+}
+
+int heap_left(int i) {
+	return 2 * i + 1;
+}
+
+int heap_right(int i) {
+	return 2 * i + 2;
+}
+
+void max_heapify(int *arr, int i, int heap_size) {
+	int largest;
+	int l = heap_left(i);
+	int r = heap_right(i);
+
+	if (l < heap_size && arr[l] > arr[i]) {
+		largest = l;
+	} else {
+		largest = i;
+	}
+
+	if (r < heap_size && arr[r] > arr[largest]) {
+		largest = r;
+	}
+
+	if (largest != i) {
+		int tmp = arr[i];
+		arr[i] = arr[largest];
+		arr[largest] = tmp;
+		max_heapify(arr, largest, heap_size);
+	}
+}
+
+void build_max_heap(int *arr, int heap_size) {
+	for (int i = heap_size / 2; i >= 0; --i) {
+		max_heapify(arr, i, heap_size);
+	}
+}
+
+void heapsort(int *arr, int heap_size) {
+	build_max_heap(arr, heap_size);
+	for (int i = heap_size - 1; i >= 1; --i) {
+		int tmp = arr[i];
+		arr[i] = arr[0];
+		arr[0] = tmp;
+		heap_size--;
+		max_heapify(arr, 0, heap_size);
+	}
+}
